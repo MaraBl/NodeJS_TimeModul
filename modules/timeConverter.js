3705fc937b86f0a8b343fdab18c9c6951fbb02b9
Marta Blaszczak <marta.blaszczak1@gmail.com>
2019-06-22 21:16:10 +0100

    Change timeConverter.js

--- modules/timeConverter.js
+++ modules/timeConverter.js
@@ -3,18 +3,16 @@ var os = require( 'os' );
 function timeConverter() {
 	var uptime = os.uptime();
 	var hours = Math.floor(uptime / 3600);
-    var minutes = Math.floor((uptime - (hours * 3600)) / 60);
-    var seconds = uptime - (hours * 3600) - (minutes * 60);
+	var minutes = Math.floor((uptime - (hours * 3600)) / 60);
+	var seconds = uptime - (hours * 3600) - (minutes * 60);
 
-    if(hours > 0) {
-        console.log('Uptime: ', hours.toFixed(0), ' hours ' + minutes.toFixed(0), ' minutes ' + seconds.toFixed(0), ' seconds' );
-    }
-    else if (minutes > 0 ) {
-        console.log('Uptime: ', minutes.toFixed(0), ' minutes ' + seconds.toFixed(0), ' seconds');
-    }
-    else if (seconds > 0 ) {
-        console.log('Uptime: ', seconds.toFixed(0), ' seconds');
-    }
+	if(hours > 0) {
+		console.log('Uptime: ', hours.toFixed(0), ' hours ' + minutes.toFixed(0), ' minutes ' + seconds.toFixed(0), ' seconds' );
+	}else if (minutes > 0 ) {
+		console.log('Uptime: ', minutes.toFixed(0), ' minutes ' + seconds.toFixed(0), ' seconds');
+	}else if (seconds > 0 ) {
+		console.log('Uptime: ', seconds.toFixed(0), ' seconds');
+	}
 }
 
 exports.printTime = timeConverter;
